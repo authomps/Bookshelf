e4ea35539372ed53ffe8527758fe2100abb6b9a2
Alexander Broom <abroom@mines.edu>
2013-11-19 20:02:13 -0700

    part 3 submission

--- access_database.php
+++ access_database.php
@@ -1,6 +1,6 @@
 <?php
 	// Connect to database
-	@ $db = new mysqli('localhost', 'root', 'indy323', 'team15');
+	@ $db = new mysqli('localhost', 'team15', 'watermelon', 'team15');
 	if ( mysqli_connect_errno() ) {
 		echo 'Error connecting to bookshelf database';
 		exit;
